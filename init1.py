9b0d1d887e64ed7e8318f9725a3e3a5a733d0b14
Anvita Hodekar <anvita@Anvitas-MBP.fios-router.home>
2017-11-29 23:29:07 -0500

    first commit

---
+++ init1.py
@@ -0,0 +1,128 @@
+#Import Flask Library
+from flask import Flask, render_template, request, session, url_for, redirect, hashlib
+import pymysql.cursors
+import hashlib
+
+#Initialize the app from Flask
+app = Flask(__name__)
+
+#Configure MySQL
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='',
+                       db='PriCoSha',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+#Define a route to hello function
+@app.route('/')
+def hello():
+	return render_template('index.html')
+
+#Define route for login
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+#Define route for register
+@app.route('/register')
+def register():
+	return render_template('register.html')
+
+#Authenticates the login
+@app.route('/loginAuth', methods=['GET', 'POST'])
+def loginAuth():
+	#grabs information from the forms
+	username = request.form['username']
+	password = request.form['password']
+
+        db_password = password 
+        h = hashlib.md5(db_password.encode())
+	#cursor used to send queries
+	cursor = conn.cursor()
+	#executes query
+	query = 'SELECT * FROM Person WHERE username = %s and password = %s'
+	cursor.execute(query, (username, h.hexdigest()))
+	#stores the results in a variable
+	data = cursor.fetchone()
+	#use fetchall() if you are expecting more than 1 data row
+	cursor.close()
+	error = None
+	if(data):
+		#creates a session for the the user
+		#session is a built in
+		session['username'] = username
+		return redirect(url_for('home'))
+	else:
+		#returns an error message to the html page
+		error = 'Invalid login or username'
+		return render_template('login.html', error=error)
+
+#Authenticates the register
+@app.route('/registerAuth', methods=['GET', 'POST'])
+
+def registerAuth():
+	#grabs information from the forms
+	username = request.form['username']
+	password = request.form['password']
+	first_name = request.form['first name']
+	last_name = request.form['last name']
+
+	#cursor used to send queries
+	cursor = conn.cursor()
+	#executes query
+	query = 'SELECT * FROM Person WHERE username = %s'
+	cursor.execute(query, (username))
+	#stores the results in a variable
+	data = cursor.fetchone()
+	#use fetchall() if you are expecting more than 1 data row
+	error = None
+	if(data):
+		#If the previous query returns data, then user exists
+		error = "This user already exists"
+		return render_template('register.html', error = error)
+	else:
+		ins = 'INSERT INTO Person VALUES(%s, %s, %s, %s)'
+		cursor.execute(ins, (username, password, first_name, last_name))
+		conn.commit()
+		cursor.close()
+		return render_template('index.html')
+
+@app.route('/home')
+def home():
+	username = session['username']
+	cursor = conn.cursor();
+	query = 'SELECT timest, id FROM Content WHERE username = %s ORDER BY ts DESC'
+	cursor.execute(query, (username))
+	data = cursor.fetchall()
+	cursor.close()
+	return render_template('home.html', username=username, posts=data)
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+        username = session['username']
+        cursor = conn.cursor();
+        content = request.form['Content']
+        timest=  time.time()
+        public = False
+        if request.form.get('public'):
+                public = True
+        query = 'INSERT INTO Content ( username, timest, content_name, public) VALUES(%s, %s, %s, %s)'
+        cursor.execute(query, (Content, username, timest, content_name, public))
+        conn.commit()
+        cursor.close()
+        return redirect(url_for('home'))
+
+
+
+@app.route('/logout')
+def logout():
+	session.pop('username')
+	return redirect('/')
+		
+app.secret_key = 'some key that you will never guess'
+#Run the app on localhost port 5000
+#debug = True -> you don't have to restart flask
+#for changes to go through, TURN OFF FOR PRODUCTION
+if __name__ == "__main__":
+	app.run('127.0.0.1', 8888, debug = True)
